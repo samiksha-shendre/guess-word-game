5315650011dcc138560b947d32accfc620ddb840
samiksha-shendre <samikshacreate@gmail.com>
2025-04-17 16:09:17 +0530

    worked with data and some mainpage functions

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
+import wordList from "../data";
 export default function MainPage() {
+  const [currentWord, setCurrentWord] = useState(() => {
+    let randomIdx = Math.floor(Math.random() * wordList.length);
+    return { word: wordList[randomIdx].word, hint: wordList[randomIdx].hint };
+  });
+
   return (
     <>
-      <h1>MainPage Component</h1>
+      <h3>{currentWord.word}</h3>
+      <p>{currentWord.hint}</p>
     </>
   );
 }
